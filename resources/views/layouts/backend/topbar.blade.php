262dc14260f44675ab452ccca85179b2573983f5
Archon <Theophyllineneo@gmail.com>
2025-11-26 04:22:27 +0700

    bug

--- resources/views/layouts/backend/topbar.blade.php
+++ resources/views/layouts/backend/topbar.blade.php
@@ -16,16 +16,6 @@
                 </ul>
 
                 <ul class="navbar-nav align-items-center right-nav-link">
-                    <li class="nav-item language">
-                        <a class="nav-link dropdown-toggle dropdown-toggle-nocaret waves-effect" data-toggle="dropdown"
-                            href="javascript:void();"><i class="fa fa-flag"></i></a>
-                        <ul class="dropdown-menu dropdown-menu-right">
-                            <li class="dropdown-item"> <i class="flag-icon flag-icon-gb mr-2"></i> English</li>
-                            <li class="dropdown-item"> <i class="flag-icon flag-icon-fr mr-2"></i> French</li>
-                            <li class="dropdown-item"> <i class="flag-icon flag-icon-cn mr-2"></i> Chinese</li>
-                            <li class="dropdown-item"> <i class="flag-icon flag-icon-de mr-2"></i> German</li>
-                        </ul>
-                    </li>
                     <li class="nav-item">
                         <a class="nav-link dropdown-toggle dropdown-toggle-nocaret" data-toggle="dropdown"
                             href="#">
@@ -37,7 +27,7 @@
                                 <a href="javaScript:void();">
                                     <div class="media">
                                         <div class="avatar"><img class="align-self-start mr-3"
-                                                src="https://via.placeholder.com/110x110" alt="user avatar"></div>
+                                                src="{{ asset('backend/assets/images/yt.jpg') }}" alt="user avatar"></div>
                                         <div class="media-body">
                                             <h6 class="mt-2 user-title">Sarajhon Mccoy</h6>
                                             <p class="user-subtitle">mccoy@example.com</p>
